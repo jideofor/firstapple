5a2a59ab43919033a3107bfa7735352d36f0ab0a
JIDEOFOR ONWUGBENU <onwugbej@JIDEOFOR-ONWUGBENUs-iMac.local>
2013-11-03 20:13:07 +0000

    initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'a6be94b46193060253f69eff37c71a9588f998f5a334d96a3361b98b9c8948036418f472e78dbf2e3f820074a66a9257be4c616bbdd4256ea7d09fe620065a83'
